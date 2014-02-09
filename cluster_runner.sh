4c92070e7bc1b959dcbdf657172ffdbc91e60134
SiegeLord <slabode@aim.com>
2014-02-09 10:37:23 -0500

    Quote the arguments so that negatives aren't treated as options

--- cluster_runner.sh
+++ cluster_runner.sh
@@ -7,7 +7,7 @@ then
 fi
 
 job_name=rosenbrock_test
-cmd="/scisoft/EPD/7.3.2/bin/python rosenbrock.py --x $1 --y $2 --wait 0.1"
+cmd="/scisoft/EPD/7.3.2/bin/python rosenbrock.py --x \"$1\" --y \"$2\" --wait 0.1"
 
 job_id=`qsub -terse -cwd -j yes -sync y -l ${QUEUE} -N ${job_name} -b yes ${cmd} | head -n 1`
 
