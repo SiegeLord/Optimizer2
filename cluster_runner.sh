991ce40a2b22ad9086592b616e3bdb2d64d57d4e
SiegeLord <slabode@aim.com>
2014-02-08 20:22:29 -0500

    Add some cluster examples

---
+++ cluster_runner.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -z "${QUEUE}" ]
+then
+	echo "You forgot to set the QUEUE"
+	exit 1
+fi
+
+job_name=rosenbrock_test
+cmd="/scisoft/EPD/7.3.2/bin/python rosenbrock.py --x $1 --y $2 --wait 0.1"
+
+job_id=`qsub -terse -cwd -j yes -sync y -l ${QUEUE} -N ${job_name} -b yes ${cmd} | head -n 1`
+
+job_id_file=${job_name}.o${job_id}
+cat ${job_id_file}
