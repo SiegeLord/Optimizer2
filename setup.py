2a6388eec251a12f6d7b683894df050e61c30cc6
SiegeLord <slabode@aim.com>
2014-03-03 18:29:05 -0500

    Fill out more fields in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(name='optimizer2',
       version='0.1',
       description='A command line optimization software',
       author='Pavel Sountsov',
+      author_email='slabode@aim.com'
       url='https://github.com/SiegeLord/Optimizer2',
       scripts=['bin/optimizer'],
+      license='LICENSE',
+      long_description=open('Readme.md').read(),
       packages=['optimizer2']
      ) 
