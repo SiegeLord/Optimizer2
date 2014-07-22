59e720aedba97488f6c505041d49115a7ecd2bea
SiegeLord <slabode@aim.com>
2014-07-21 20:48:18 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='optimizer2',
-      version='0.1',
+      version='0.2',
       description='A command line optimization software',
       author='Pavel Sountsov',
       author_email='slabode@aim.com',
       url='https://github.com/SiegeLord/Optimizer2',
       scripts=['bin/optimizer'],
-      license='LICENSE',
+      license='GPL v3',
       packages=['optimizer2']
      ) 
